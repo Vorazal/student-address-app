9658fa6954736d3b2dff52302f1137c18e0c99c2
ICU <hristo_laz@mail.bg>
2022-02-10 16:11:00 +0200

    spring initializr files added

---
+++ src/main/java/com/studentaddressapp/StudentAddressAppApplication.java
@@ -0,0 +1,13 @@
+package com.studentaddressapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAddressAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAddressAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentaddressapp/StudentAddressAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentaddressapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAddressAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
